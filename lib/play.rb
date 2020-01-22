03a24a905814cc03726e22abe003275911dd96f8
confident-token-1019 <theoonetj@gmail.com>
2020-01-22 18:30:20 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -50,19 +50,25 @@ end
 # Define your play method below
 # Define your play method below
 
+
 def play(board)
-i = 0
-while i < 9
-  i += 1
-  turn(board)
-end
+  turns = 3
+  counter = 0
+  loop do
+    counter = counter + 1
+    turn(board)
+    if counter >= turns
+      puts "You only get #{turns} turns baby!"
+    end
+  end
 end
 
+#else ask for input again until you get valid input
+#_________________________________________________
 
 board = ["X"," "," "," "," "," "," "," "," "]
 puts "Welcome to Tic Tac Toe!"
 puts display_board(board)
 #turn(board)
 play(board)
-
 
